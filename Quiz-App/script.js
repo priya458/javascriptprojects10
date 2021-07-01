bcd44777c9742535e000e370eba5a2e217c46583
Priyanka Gajabapu Gadhave <piyankagadhave0303@gmail.com>
2021-07-01 04:31:33 -0700

    modified in script

--- Quiz-App/script.js
+++ Quiz-App/script.js
@@ -6,21 +6,23 @@ const quizData=[
            c:'26',
            d:'110',
            correct:'c'
-    },{
+    },
+    {
         question:'what is the best pogramming language in 2019?',
         a:'java',
         b:'C',
         c:'python',
         d:'JavaScript',
-        correct:'d',
+        correct:'d'
     
-    },{
+    },
+    {
         question:'who is the resident of us?',
         a:'Floria POP',
         b:'Donald Trump',
         c:'John saldano',
         d:'Mihal Andrei',
-        correct:'b',
+        correct:'b'
     },
     {
         question:'what does HTML stand for?',
@@ -28,7 +30,7 @@ const quizData=[
         b:'Donald Trump',
         c:'Cascading Stylesheet',
         d:'JSON object Notaion',
-        correct:'a',
+        correct:'a'
     },
 
     {
@@ -37,40 +39,76 @@ const quizData=[
         b:'2019',
         c:'2018',
         d:'none',
-        correct:'d',
+        correct:'d'
     }
 
 
-]
-const questionE1=documwnt.getElementById('question');
+];
+const questionE1 = document.getElementById("question");
 const a_text=document.getElementById('a_text');
 const b_text = document.getElementById('b_text');
 const c_text = document.getElementById('c_text');
 const d_text = document.getElementById('d_text');
+const  addBtn = document.getElementById("addBtn");
 
-
+const answers=document.querySelectorAll(".answer");
+const showScore=document.querySelector("#showScore");
 let currentQuiz=0;
-loadQuiz();
+let score=0;
 
-function loadQuiz(){
-const currentQuizDate=quizData(currentquiz);
+
+const loadQuiz=()=>{
+
+    const currentQuizData=quizData[currentQuiz];
+
 questionE1.innerText=currentQuizData.question;
-a_text.innnerText=currentQuizData.a;
-b_text.innnerText=currentQuizData.b;
-c_text.innnerText=currentQuizData.c;
-d_text.innnerText=currentQuizData.d;
+
+a_text.innerText=currentQuizData.a;
+b_text.innerText=currentQuizData.b;
+c_text.innerText=currentQuizData.c;
+d_text.innerText=currentQuizData.d;
+
 }
 
-submitBtn.addEventListener('click',()=>{
+loadQuiz();
+
+const getCheckAnswer=()=>{
+ 
+ let answer;
+    answers.forEach((currAnsElm) =>{
+        if(currAnsElm.checked){
+        answer=currAnsElm.id;
+        }
+        
+    });
+    return answer;
+};
+const disSelectAll=()=>{
+    answers.forEach((currAnsElm)=>currAnsElm.checked=false);
+}
+
+addBtn.addEventListener('click',()=>{
+ const  checkedanswer=getCheckAnswer();
+ console.log(checkedanswer);
+
+ if( checkedanswer===quizData[currentQuiz].correct){
+    score++;
+}
+
 currentQuiz++;
+disSelectAll();
 
 if(currentQuiz<quizData.length){
     loadQuiz();
 }
 else{
-alert('You Finished get yourself an orange lemonde');
+      showScore.innerHTML=`
+      <h3>You scored ${score}/${quizData.length}</h3>
+      <button class="btn" onclick="location.reload()">play again</button>`;
+
+      showScore.classList.remove('scorearea');
 }
 
 
-})                                      
+}) ;                                     
 
